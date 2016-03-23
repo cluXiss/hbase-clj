61be1bb5802a968d5ce07e79fe4b2b81a2ea269e
clyce <epi.clyce@gmail.com>
2016-03-23 18:42:23 +0800

    rearranged namespaces and updated docstrings

--- src/hbase_clj/driver.clj
+++ src/hbase_clj/driver.clj
@@ -29,9 +29,9 @@
   "Generate a HBase Connection config according to the options, 
    params should be pairs, e.g: 
    (gen-config 
-   \"hbase.zookeeper.quorum\" \"localhost\"
-  ...
-   ...)"
+     \"hbase.zookeeper.quorum\" \"localhost\"
+     ...
+     ...)"
 
   [& options]
 
@@ -41,21 +41,6 @@
     (reset! latest-config conf)
     conf))
 
-(defmacro defhbase 
-
-  "Defines a HBase Connection config with specified var name according to the options, 
-   params should be pairs, e.g: 
-   (defhbase main-hb
-   \"hbase.zookeeper.quorum\" \"localhost\"
-  ...
-   ...)"
-
-
-  [name & options]
-
-  `(def ^HBaseConfiguration ~name 
-     (gen-config ~@options)))
-
 (defn connect!
   [cfg]
   (if-not (@connections cfg) 
@@ -63,52 +48,3 @@
            assoc cfg
            (HConnectionManager/createConnection cfg)))
   (@connections cfg))
-
-(defrecord HBTable-schema
-  [hb table-name id-type families])
-
-(defmacro def-hbtable 
-
-  "defines a htable \"schema\", 
-   types could be one of #{:int :long :string :keyword :raw}
-   and could create custom types via codec.defcodec
-   families could be like: 
-
-   --------------Snippet--------------
-   ...
-   ....
-   :info {:--ktype :keyword
-          :--vtype :string 
-          :age :long}
-
-   :following {:--ktype :string
-               :--vtype :string}
-   ....
-   ...
-   -----------------------------------
-
-   where `--ktype` means the type of all keys in the family, default as `:keyword`
-         `--vtype` means the default type of values in the family, default as `string`
-         and other k-v stands for a specified value type given a key, instead of the `--vtype`"
-
-  [table {:keys [id-type table-name hbase]
-          :or {id-type :string}}
-   & families] 
-
-  `(def ^HBTable-schema ~table 
-     (HBTable-schema. 
-       (or ~hbase @latest-config)
-       (or ~table-name (name ~table))
-       ~id-type
-       (hash-map ~@families))))
-
-(defmacro with-table 
-  [table & form]
-  `(let [hb#         (:hb ~table)
-         table-name# (:table-name ~table)]
-     (binding 
-       [^HTableInterface *table* (.getTable (connect! hb#) 
-                                            (Bytes/toBytes table-name#))
-        *schema* (select-keys ~table [:id-type :families])]
-       ~@form
-       (.close *table*))))
